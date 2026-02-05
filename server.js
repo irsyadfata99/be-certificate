2561a235e97d2bd745f340f7b59308deb04c4f19
irsyadfata99 <irsyadfata99@gmail.com>
2026-02-05 16:04:20 +0700

    fix cors

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
   "http://localhost:3001",
   "http://localhost:5173", // Vite default
   "http://localhost:5174",
+  "http://127.0.0.1:5500",
   "http://localhost:8080", // Vue default
   // Add your production domain here:
   // "https://yourdomain.com",
